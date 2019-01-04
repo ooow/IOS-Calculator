118c44a7d8193838a41238bd7f208cb154a40e0a
Goga Tirkiya <tirkiya@icloud.com>
2019-01-04 02:09:53 +0100

    Implement logic of simple operations

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,32 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    var numberFromScreen: Double = 0;
-    @IBOutlet weak var result: UILabel!
+    
+    var actualNumber: Double = 0;
+    var storedNumber: Double = 0;
+    var operationType: Int = 0;
+    var isPressedOpration: Bool = false;
+    @IBOutlet weak var result: UILabel!;
     
     @IBAction func digits(_ sender: UIButton) {
-        result.text = result.text! + String(sender.tag)
-        numberFromScreen = Double(result.text!)!
+        if isPressedOpration == true {
+            result.text = String(sender.tag);
+            isPressedOpration = false;
+        } else {
+            result.text = result.text! + String(sender.tag);
+        }
+        actualNumber = Double(result.text!)!;
     }
     
     @IBAction func buttons(_ sender: UIButton) {
         if result.text != "" {
             switch sender.tag {
             case 10: // Clean
-                print("Clean")
+                clean();
             case 11: // Division
-                print("Division")
+                pressOperation(operation: "/", tag: sender.tag);
             case 12: // Multiplication
-                print("Multiplication")
+                pressOperation(operation: "*", tag: sender.tag);
             case 13: // Subtraction
-                print("Subtraction")
+                pressOperation(operation: "-", tag: sender.tag);
             case 14: // Summation
-                print("Summation")
+                pressOperation(operation: "+", tag: sender.tag);
+            case 15: // Equal
+                calculate(operation: operationType);
             default:
-                print()
+                break;
             }
         }
+    }
+    
+    func pressOperation(operation: String, tag: Int){
+        storedNumber = Double(result.text!)!;
+        result.text = operation;
+        operationType = tag;
+        isPressedOpration = true;
+    }
+    
+    func calculate(operation: Int){
+        switch operation {
+        case 11: // Division
+            result.text = String(storedNumber / actualNumber)
+        case 12: // Multiplication
+            result.text = String(storedNumber * actualNumber)
+        case 13: // Subtraction
+            result.text = String(storedNumber - actualNumber)
+        case 14: // Summation
+            result.text = String(storedNumber + actualNumber)
+        default:
+            break;
+        }
+        isPressedOpration = true;
+    }
+    
+    func clean(){
+        result.text = "";
+        storedNumber = 0;
+        actualNumber = 0;
+        operationType = 0;
     }
     
     override func viewDidLoad() {
@@ -42,4 +82,3 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 }
-
